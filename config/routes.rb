17ddebadf0065dac0cec267b0aa6805eb5567b63
mizouchi-n <mangetsu212@icloud.com>
2020-11-12 11:53:50 +0900

    ルーティング変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'topics#index'
+
   get 'topics/index'
   get 'topics/show/:id' => 'topics#show', as: :topics_show
   post 'topics/create' => 'topics#create'
